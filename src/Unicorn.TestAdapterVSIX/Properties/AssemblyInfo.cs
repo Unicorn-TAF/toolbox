da29243ca47df97bbf7b54a969950d2d5509a0c6
Vitaliy Dobriyan <dobriy88@yandex.ru>
2020-02-09 13:43:53 +0400

    TestAdapterVSIX: corrected assembly info

--- src/Unicorn.TestAdapterVSIX/Properties/AssemblyInfo.cs
+++ src/Unicorn.TestAdapterVSIX/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Unicorn.TestAdapterInstallVSIX2017")]
+[assembly: AssemblyTitle("Unicorn.TestAdapterVSIX")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Vitaliy Dobriyan")]
-[assembly: AssemblyProduct("Unicorn.TestAdapterInstallVSIX (VS 2017)")]
+[assembly: AssemblyProduct("Unicorn.TestAdapterVSIX")]
 [assembly: AssemblyCopyright("Copyright © 2019 Vitaliy Dobriyan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
