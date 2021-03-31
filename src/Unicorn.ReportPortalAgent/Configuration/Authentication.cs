5df0a72cefcec9c1c8b4b25271304386930dbf4e
Vitaliy Dobriyan <dobriy88@yandex.ru>
2021-04-01 01:18:05 +0300

    Added Unicorn.ReportPortalAgent

---
+++ src/Unicorn.ReportPortalAgent/Configuration/Authentication.cs
@@ -0,0 +1,13 @@
+﻿namespace Unicorn.ReportPortalAgent.Configuration
+{
+    /// <summary>
+    /// Report portal authentication object.
+    /// </summary>
+    public class Authentication
+    {
+        /// <summary>
+        /// Gets or sets RP user UUID to authenticate.
+        /// </summary>
+        public string Uuid { get; set; }
+    }
+}
