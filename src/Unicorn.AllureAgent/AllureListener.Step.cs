ea1cf3796fbbeb1527b688d2e478a19b190d580b
Vitaliy Dobriyan <dobriy88@yandex.ru>
2021-04-01 00:41:04 +0300

    implemented allure agent mvp

---
+++ src/Unicorn.AllureAgent/AllureListener.Step.cs
@@ -0,0 +1,45 @@
+﻿using Allure.Commons;
+using System;
+using System.Reflection;
+using Unicorn.Taf.Core.Steps;
+
+namespace Unicorn.AllureAgent
+{
+    public partial class AllureListener
+    {
+        private string stepGuid = null;
+
+        internal void StartStep(MethodBase method, object[] arguments)
+        {
+            try
+            {
+                stepGuid = Guid.NewGuid().ToString();
+
+                var result = new StepResult()
+                {
+                    name = StepsUtilities.GetStepInfo(method, arguments),
+                    status = Status.passed
+                };
+
+                AllureLifecycle.Instance.StartStep(testGuid, stepGuid, result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception in StartStep '{0}'." + Environment.NewLine + e);
+            }
+        }
+
+        internal void FinishStep(MethodBase method, object[] arguments)
+        {
+            try
+            {
+                AllureLifecycle.Instance.StopStep(stepGuid);
+                stepGuid = null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception in FinishStep" + Environment.NewLine + e);
+            }
+        }
+    }
+}
---
+++ src/Unicorn.AllureAgent/AllureReporterInstance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Unicorn.Taf.Core.Steps;
+using Unicorn.Taf.Core.Testing;
+
+namespace Unicorn.AllureAgent
+{
+    public sealed class AllureReporterInstance : IDisposable
+    {
+        private readonly AllureListener _listener;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AllureReporterInstance"/> class.<br/>
+        /// Automatic subscribtion to all test events.
+        /// </summary>
+        public AllureReporterInstance()
+        {
+            _listener = new AllureListener();
+
+            Test.OnTestStart += _listener.StartSuiteMethod;
+            Test.OnTestFinish += _listener.FinishSuiteMethod;
+            Test.OnTestSkip += _listener.SkipSuiteMethod;
+
+            SuiteMethod.OnSuiteMethodStart += _listener.StartSuiteMethod;
+            SuiteMethod.OnSuiteMethodFinish += _listener.FinishSuiteMethod;
+
+            TestSuite.OnSuiteStart += _listener.StartSuite;
+            TestSuite.OnSuiteFinish += _listener.FinishSuite;
+
+            StepsEvents.OnStepStart += _listener.StartStep;
+            StepsEvents.OnStepFinish += _listener.FinishStep;
+        }
+
+        public void Dispose()
+        {
+            Test.OnTestStart -= _listener.StartSuiteMethod;
+            Test.OnTestFinish -= _listener.FinishSuiteMethod;
+            Test.OnTestSkip -= _listener.SkipSuiteMethod;
+
+            SuiteMethod.OnSuiteMethodStart -= _listener.StartSuiteMethod;
+            SuiteMethod.OnSuiteMethodFinish -= _listener.FinishSuiteMethod;
+
+            TestSuite.OnSuiteStart -= _listener.StartSuite;
+            TestSuite.OnSuiteFinish -= _listener.FinishSuite;
+
+            StepsEvents.OnStepStart -= _listener.StartStep;
+            StepsEvents.OnStepFinish -= _listener.FinishStep;
+        }
+    }
+}
