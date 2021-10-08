9b7d3ba370f21dd0964b1dd79b257ae58a48b9a9
Vitaliy Dobriyan <dobriy88@yandex.ru>
2021-10-08 23:05:53 +0300

    ConsoleRunner: updated ascii logo, removed unused dependencies, code segregation

---
+++ src/Unicorn.ConsoleRunner/Reporter.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+using Unicorn.Taf.Core.Engine;
+using Unicorn.Taf.Core.Engine.Configuration;
+using Unicorn.Taf.Core.Testing;
+
+namespace Unicorn.ConsoleRunner
+{
+    internal static class Reporter
+    {
+        private static readonly string Delimiter = new string('-', Console.WindowWidth);
+
+        internal static void ReportResults(LaunchOutcome outcome)
+        {
+            StringBuilder header = new StringBuilder()
+                .AppendLine("\n\n")
+                .AppendLine(Delimiter)
+                .AppendLine($"Tests run {outcome.RunStatus}")
+                .AppendLine();
+
+            Console.ForegroundColor = outcome.RunStatus.Equals(Status.Passed) ?
+                ConsoleColor.Green :
+                ConsoleColor.Red;
+
+            Console.Write(header.ToString());
+
+            int passedTests = 0;
+            int skippedTests = 0;
+            int failedTests = 0;
+
+            foreach (SuiteOutcome suiteOutcome in outcome.SuitesOutcomes)
+            {
+                failedTests += suiteOutcome.FailedTests;
+                skippedTests += suiteOutcome.SkippedTests;
+                passedTests += suiteOutcome.PassedTests;
+            }
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write($"Passed tests: {passedTests}    ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($"Failed tests: {failedTests}    ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"Skipped tests: {skippedTests}");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($" Total tests: {passedTests + failedTests + skippedTests}");
+        }
+
+        internal static void ReportHeader(string assemblyPath)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+
+            Console.WriteLine(ResourceAsciiLogo.Logo);
+            Console.WriteLine();
+
+            Console.WriteLine(Delimiter);
+            Console.WriteLine();
+            Console.WriteLine("Tests assembly: " + assemblyPath);
+            Console.WriteLine();
+            Console.WriteLine(Config.GetInfo());
+            Console.WriteLine(Delimiter);
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
