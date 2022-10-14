b2841ff188825973e0a595169c4897cccdf48253
Vitaliy Dobriyan <dobriy88@yandex.ru>
2022-10-14 09:15:32 +0300

    new isolation code for console runner, arguments parsing improvements

--- src/Unicorn.ConsoleRunner/Reporter.cs
+++ src/Unicorn.ConsoleRunner/Reporter.cs
@@ -3,6 +3,7 @@ using System.Text;
 using Unicorn.Taf.Core.Engine;
 using Unicorn.Taf.Core;
 using Unicorn.Taf.Core.Testing;
+using System.Linq;
 
 namespace Unicorn.ConsoleRunner
 {
@@ -12,51 +13,60 @@ namespace Unicorn.ConsoleRunner
 
         internal static void ReportResults(LaunchOutcome outcome)
         {
+            TimeSpan ts = DateTime.Now - outcome.StartTime;
+
             StringBuilder header = new StringBuilder()
                 .AppendLine("\n\n")
                 .AppendLine(Delimiter)
-                .AppendLine($"Tests run {outcome.RunStatus}")
+                .AppendFormat("Tests run {0}", outcome.RunStatus)
+                .AppendFormat(" (duration {0}h {1}m {2}s {3}ms)\n", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds)
                 .AppendLine();
 
-            Console.ForegroundColor = outcome.RunStatus.Equals(Status.Passed) ?
+            Console.ForegroundColor = outcome.RunStatus.Equals(Status.Passed) ? 
                 ConsoleColor.Green :
                 ConsoleColor.Red;
 
             Console.Write(header.ToString());
 
-            int passedTests = 0;
-            int skippedTests = 0;
-            int failedTests = 0;
+            int passedTests = outcome.SuitesOutcomes.Sum(o => o.PassedTests);
+            int skippedTests = outcome.SuitesOutcomes.Sum(o => o.SkippedTests);
+            int failedTests = outcome.SuitesOutcomes.Sum(o => o.FailedTests);
 
-            foreach (SuiteOutcome suiteOutcome in outcome.SuitesOutcomes)
-            {
-                failedTests += suiteOutcome.FailedTests;
-                skippedTests += suiteOutcome.SkippedTests;
-                passedTests += suiteOutcome.PassedTests;
-            }
+            Console.ForegroundColor = passedTests == 0 ? 
+                ConsoleColor.DarkGray : 
+                ConsoleColor.DarkGreen;
 
-            Console.ForegroundColor = passedTests == 0 ? ConsoleColor.DarkGray : ConsoleColor.DarkGreen;
             Console.Write($"Passed tests: {passedTests}    ");
 
-            Console.ForegroundColor = failedTests == 0 ? ConsoleColor.DarkGray : ConsoleColor.DarkRed;
+            Console.ForegroundColor = failedTests == 0 ? 
+                ConsoleColor.DarkGray : 
+                ConsoleColor.DarkRed;
+
             Console.Write($"Failed tests: {failedTests}    ");
 
-            Console.ForegroundColor = skippedTests == 0 ? ConsoleColor.DarkGray : ConsoleColor.DarkYellow;
+            Console.ForegroundColor = skippedTests == 0 ? 
+                ConsoleColor.DarkGray : 
+                ConsoleColor.DarkYellow;
+
             Console.WriteLine($"Skipped tests: {skippedTests}");
 
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine($" Total tests: {passedTests + failedTests + skippedTests}");
+            Console.WriteLine($"-----> Total: {passedTests + failedTests + skippedTests}");
         }
 
-        internal static void ReportHeader(string assemblyPath)
+        internal static void ReportHeader(string assemblyPath, bool hideLogo)
         {
             Console.ForegroundColor = ConsoleColor.DarkGray;
+            
+            if (!hideLogo)
+            {
+                Console.WriteLine(ResourceAsciiLogo.Logo);
+                Console.WriteLine();
 
-            Console.WriteLine(ResourceAsciiLogo.Logo);
-            Console.WriteLine();
-
-            Console.WriteLine(Delimiter);
-            Console.WriteLine();
+                Console.WriteLine(Delimiter);
+                Console.WriteLine();
+            }
+            
             Console.WriteLine("Tests assembly: " + assemblyPath);
             Console.WriteLine();
             Console.WriteLine(Config.GetInfo());
