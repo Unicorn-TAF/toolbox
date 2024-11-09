8b9785931a97c8a61292b54370bc182d7aa1fc5a
Vitaliy Dobriyan <dobriy88@yandex.ru>
2024-11-09 22:19:07 +0300

    renamings, improved publish script

---
+++ tools/publish.bat
@@ -0,0 +1,11 @@
+@echo off
+
+set VERSION=%1
+set OUT_DIR=%2\unicorn-toolbox
+set PROJ_PATH=%cd%\..\src\Unicorn.Toolbox\Unicorn.Toolbox.csproj
+
+for %%v in (net462, net6.0-windows) do (
+	dotnet publish %PROJ_PATH% --configuration Release --framework %%v --output %OUT_DIR%\%%v -p:VersionPrefix=%VERSION%
+	del %OUT_DIR%\net462\AspectInjector.Broker.dll
+)
+
