71fb6aad27d34ce0a3901a3145dc05f3f55ff92f
Vitaliy Dobriyan <dobriy88@yandex.ru>
2024-12-08 22:36:35 +0300

    small tooling files changes

--- publish.bat
+++ publish.bat
@@ -2,7 +2,7 @@
 
 set VERSION=%1
 set OUT_DIR=%2\unicorn-toolbox
-set PROJ_PATH=%cd%\..\src\Unicorn.Toolbox\Unicorn.Toolbox.csproj
+set PROJ_PATH=src\Unicorn.Toolbox\Unicorn.Toolbox.csproj
 
 for %%v in (net462, net6.0-windows) do (
 	dotnet publish %PROJ_PATH% --configuration Release --framework %%v --output %OUT_DIR%\%%v -p:VersionPrefix=%VERSION%
